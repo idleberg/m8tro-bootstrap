36d3d4ee249f715e80fac8787208d50f063028dc
idleberg <jan@idleberg.com>
2014-06-02 22:06:35 +0200

    Update files

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo $1
+echo
+echo M8tro 3.1.1-alpha
+echo =================
+
 if [ -z "$1" ]
 then
-	THEME=m8tro
+	for THEME in 'src/themes'/*
+	do
+		THEME_BASE=$(basename $THEME)
+		echo Compiling style-sheet for $THEME_BASE, please wait.
+		lessc -x src/themes/$THEME_BASE/build.less dist/$THEME_BASE.min.css
+	done
 else
 	THEME=$1
+	echo Compiling style-sheet for $THEME, please wait.
+	lessc -x src/themes/$THEME/build.less dist/$THEME.min.css
 fi
-
-echo Compiling style sheet for $THEME, please wait... 
-lessc -x src/themes/$THEME/build.less dist/$THEME.min.css
+echo Completed.
