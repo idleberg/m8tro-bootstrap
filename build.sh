ec6ca3321e13386266ea07c7c371533e8e4fe39b
idleberg <jan@idleberg.com>
2014-06-02 19:37:57 +0200

    Update files

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
- lessc -x src/build.less dist/m8tro.min.css
+
+echo $1
+if [ -z "$1" ]
+then
+	THEME=m8tro
+else
+	THEME=$1
+fi
+
+echo Compiling style sheet for $THEME, please wait... 
+lessc -x src/themes/$THEME.build.less dist/$THEME.min.css
